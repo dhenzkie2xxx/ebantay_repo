59621a8815ba4c6be7c8e7b2d6efac10a67f9cb9
Dhen Marc Ginos <ginosdhenmarc@gmail.com>
2026-03-03 01:13:12 +0800

    added cors

--- dashboard_summary.php
+++ dashboard_summary.php
@@ -1,4 +1,24 @@
 <?php
+$allowed = [
+  "http://localhost:5173",
+  "https://ebantay.top.gen.in"
+];
+
+$origin = $_SERVER['HTTP_ORIGIN'] ?? "";
+if (in_array($origin, $allowed, true)) {
+  header("Access-Control-Allow-Origin: $origin");
+  header("Vary: Origin");
+}
+
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Content-Type: application/json; charset=utf-8");
+
+if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
+  http_response_code(200);
+  exit;
+}
+
 require_once __DIR__ . "/db.php";
 require_once __DIR__ . "/require_admin.php";
 
