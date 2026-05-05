fd666eec73a7e544ef57578b31b7fd427dcef2e5
Dhen Marc Ginos <ginosdhenmarc@gmail.com>
2026-05-05 08:19:47 +0800

    run incident seeds v2

--- run_seed_hotspot_recalc_v2.php
+++ run_seed_hotspot_recalc_v2.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . "/require_admin.php";
+require_once __DIR__ . "/db.php"; // or your DB connection file
 require_once __DIR__ . "/hotspot_lib.php";
 
 header("Content-Type: application/json; charset=UTF-8");
